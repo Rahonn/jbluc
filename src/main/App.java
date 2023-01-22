d747c3ca3ae19e5fa7686d2d71b892a4c351f64b
aixoio <aixoio@yahoo.com>
2023-01-23 10:17:10 +1100

    Make App class

---
+++ src/main/App.java
@@ -0,0 +1,23 @@
+package main;
+
+public class App {
+
+    private String[] args;
+
+
+    public App(String[] args) {
+
+        this.args = args;
+
+
+    }
+
+    public void run() {
+
+        System.out.println("Hello World!");
+
+
+    }
+
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
+        App app = new App(args);
+
+        app.run();
 
 
     }
