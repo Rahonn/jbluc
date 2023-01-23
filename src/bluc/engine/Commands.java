43f887135955753a159d60a59b08a97a6c20b555
aixoio <aixoio@yahoo.com>
2023-01-23 17:58:20 +1100

    Add input command

--- src/bluc/engine/Commands.java
+++ src/bluc/engine/Commands.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 import bluc.engine.codes.Comment;
 import bluc.engine.codes.ErrorOut;
+import bluc.engine.codes.Input;
 import bluc.engine.codes.Print;
 import bluc.engine.codes.PrintVar;
 import bluc.engine.codes.SetVar;
@@ -34,6 +35,12 @@ public class Commands {
         if (Pattern.compile("^PRINTVAR ", Pattern.MULTILINE).matcher(line).find()) {
 
             return new PrintVar(line, index);
+
+        }
+
+        if (Pattern.compile("^INPUT ", Pattern.MULTILINE).matcher(line).find()) {
+
+            return new Input(line, index);
 
         }
 
---
+++ src/bluc/engine/codes/Input.java
@@ -0,0 +1,28 @@
+package bluc.engine.codes;
+
+import java.util.Scanner;
+
+import bluc.engine.codes.base.Basecode;
+import bluc.engine.vars.Var;
+import main.App;
+
+public class Input extends Basecode {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private String varname;
+
+    public Input(String line, Integer index) {
+        super(line, index);
+        this.varname = this.line.substring(6);
+    }
+
+    @Override
+    public boolean run() {
+
+        App.getvarmamager().setVar(this.varname, new Var<String>(Input.scanner.nextLine()));
+
+        return true;
+    }
+
+}
